e4813bd87ec217803c19c1439bb4af16bbf99dfc
4utism019 <148667691+4utism019@users.noreply.github.com>
2023-12-24 01:30:46 -0600

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,8 @@
+// Mini proyecto de broma
+
+var boton = document.getElementById("botonsin");
+
+boton.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Te quelo");
+});
